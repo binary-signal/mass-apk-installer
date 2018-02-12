404ad55e42bcbb2fb6805fca7d4679ebee2b6ed1
Evan <binary-signal@users.noreply.github.com>
2018-02-12 07:42:17 +0200

    Update restore.sh

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 
-
+# restore from zip file
 ./apk_mass_install -i archive.zip
+
+# restore from folder
 ./apk_mass_install -i folder/path
+
+# restore from encrypted zip file
 ./apk_mass_install -i encrypted.zip
