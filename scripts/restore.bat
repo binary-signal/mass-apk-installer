bbfeba0538efc9ca0f9db9fc327ef1970ad9599b
Evan <binary-signal@users.noreply.github.com>
2018-02-12 07:43:14 +0200

    Update restore.bat

--- scripts/restore.bat
+++ scripts/restore.bat
@@ -1,6 +1,11 @@
 ::Run.bat
-::Automates installation for apk files
+::mass apk installer v3.0
 
+# restore from zip file
 apk_mass_install -i archive.zip
+
+# restore from folder
 apk_mass_install -i folder/path
+
+# restore form encrypted zip file
 apk_mass_install -i encrypted.zip
