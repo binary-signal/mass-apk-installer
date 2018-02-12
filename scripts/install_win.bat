237a1bb30076246ea73554d36f546837ea9fdd1f
Evan <binary-signal@users.noreply.github.com>
2018-02-12 07:41:02 +0200

    Update and rename install_win.bat to restore.bat

--- scripts/install_win.bat
+++
@@ -1,4 +0,0 @@
-::Run.bat
-::Automates installation for apk files
-ADB\adb kill-server
-apk_mass_install -m install -s backup
---
+++ scripts/restore.bat
@@ -0,0 +1,6 @@
+::Run.bat
+::Automates installation for apk files
+
+apk_mass_install -i archive.zip
+apk_mass_install -i folder/path
+apk_mass_install -i encrypted.zip
