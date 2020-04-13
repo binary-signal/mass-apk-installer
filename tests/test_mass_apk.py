354c00059d2628b2822f288662b8c834ffbc5280
Evan <binary-signal@users.noreply.github.com>
2020-04-13 08:45:22 +0200

    added tests

--- tests/test_mass_apk.py
+++ tests/test_mass_apk.py
@@ -1,5 +1,19 @@
+from unittest import TestCase
+
 from massapk import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+class TestAdb(TestCase):
+    def test_start_server(self):
+        from massapk import adb
+
+        try:
+            adb.stop_server()
+            adb.start_server()
+            adb.stop_server()
+        except Exception:
+            self.fail()
