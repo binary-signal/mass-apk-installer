a2364ccbee2e7e36091c85ac92d96c68bbf3557d
evan <evan@ouzo.local>
2020-10-28 18:55:28 +0100

    more tests for cli

--- tests/test_mass_apk.py
+++ tests/test_mass_apk.py
@@ -1,11 +1,12 @@
 """Test module for mass apk."""
-
+import  pytest
+from click.testing import CliRunner
 from mass_apk import __version__
+from mass_apk import cli
 
 
 def test_version():
     assert __version__ == '0.3.1'
-
 
 
 def test_start_server():
@@ -21,6 +22,7 @@ def test_start_server():
         errored = True
     assert not errored
 
+
 def test_platform():
     import mass_apk
 
@@ -31,3 +33,20 @@ def test_exec_command():
     from mass_apk.adb import Adb
 
     output = Adb().exec_command("help")
+
+
+@pytest.fixture(scope="module")
+def runner():
+    return CliRunner()
+
+
+def test_backup_command_no_params(runner):
+
+    response = runner.invoke(cli.backup, input="")
+    assert response.exit_code == 2
+
+
+def test_restore_command_no_params(runner):
+
+    response = runner.invoke(cli.restore, input="")
+    assert response.exit_code == 2
