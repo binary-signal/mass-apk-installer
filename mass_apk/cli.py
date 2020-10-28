92fdd75a04f8a599b54ebacf5c3bfdc030070094
evan <evan@ouzo.local>
2020-10-28 21:05:33 +0100

    small refactoring

--- mass_apk/cli.py
+++ mass_apk/cli.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 from time import sleep
 import pathlib
-from typing import List, Optional, Union
+from typing import  Union
 
 import click
 
@@ -12,7 +12,7 @@ from mass_apk import _logger as log
 from mass_apk import adb
 from mass_apk.adb import Adb
 from mass_apk.apk import AdbError, map_apk_paths
-from mass_apk.exceptions import MassApkFileNotFoundError
+from mass_apk.exceptions import MassApkFileNotFoundError, MassApkError
 from mass_apk.helpers import elapsed_time
 from mass_apk.ziptools import unzipify, zipify
 
@@ -69,7 +69,7 @@ from mass_apk.ziptools import unzipify, zipify
 
 
 def main():
-    """Main entry for mass apk when invoked as a script."""
+    """Invoke main entry point for mass apk."""
     try:
         cli()
     except KeyboardInterrupt:
@@ -126,55 +126,54 @@ def backup(path: os.PathLike, list_flag: str, archive: bool):
         path = pathlib.Path(path)
         if path.exists():
             click.echo("Back up path already exists")
+            sys.exit(0)
 
-    with Adb() as adb_session:
-        # wait for adb-server to detect phone
-        log.info("Waiting for device to be connected via USB...")
-        wait_count = 0
-        while adb_session.state is not adb_session.ConnectionState.CONNECTED:
-            sleep(1)
-            wait_count += 1
-            if wait_count == 5:
-                return
+    server = Adb(auto_start=True)
 
-        log.info("Device connected")
+    if server.state is not server.ConnectionState.CONNECTED:
+        click.echo("Device not connected.")
+        server.stop_server()
+        sys.exit(0)
+    log.info("Device connected")
 
-        # get user installed packages
-        apks = adb_session.list_device(list_flag)
+    # get user installed packages
+    apks = server.list_device(list_flag)
 
-        log.info("Discovering apk paths, this may take a while...")
-        # get full path on the android filesystem for each installed package
+    log.info("Discovering apk paths, this may take a while...")
+    # get full path on the android filesystem for each installed package
 
-        parsed_paths = map_apk_paths(apks)
+    parsed_paths = map_apk_paths(apks)
 
-        log.info("Found %s installed packages", len(apks))
+    log.info("Found %s installed packages", len(apks))
 
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        click.echo("Back up destination already exists", err=True)
+        sys.exit(-1)
+
+    for progress, item in enumerate(parsed_paths, 1):
+        msg = f"[{progress:4}/{len(parsed_paths):4}] pulling ... {item.name}"
+        log.info(msg)
         try:
-            os.makedirs(path)
-        except FileExistsError:
-            log.error("Back up destination already exists")
+            server.pull(item.fullpath)  # get apk from device
+        except AdbError as error:
+            click.echo("Error during pulling {0}\n{1}".format(item, str(error)), err=True)
+            server.stop_server()
             sys.exit(-1)
 
-        for progress, item in enumerate(parsed_paths, 1):
-            msg = f"[{progress:4}/{len(parsed_paths):4}] pulling ... {item.name}"
-            log.info(msg)
-            try:
-                adb_session.pull(item.fullpath)  # get apk from device
-            except AdbError:
-                log.error(f"Meow here is a warning for `{item.name}` apk")
-                continue
-
-            # all apks retrieved with adb are saved under the same name,  `base.apk`
-            # time to use a handy AbsPath named tuple to set correct name to apk pulled with adb
-            # this is the correct name of the apk relative to the filesystem
-            dest = os.path.join(path, f"{item.name}.apk")
-            shutil.move("base.apk", dest)
+        # all apks retrieved with adb are saved under the same name,  `base.apk`
+        # time to use a handy AbsPath named tuple to set correct name to apk
+        # pulled with adb this is the correct name of the apk relative to the filesystem
+        dest = os.path.join(path, f"{item.name}.apk")
+        shutil.move("base.apk", dest)
 
     if archive:
         log.info(f"Creating zip archive: {path}.zip, this may take a while")
         zipify(path, path.parent / (path.name + ".zip"))
         shutil.rmtree(path)
 
+    server.stop_server()
     log.info("Back up done.")
 
 
@@ -200,46 +199,56 @@ def restore(path: Union["os.PathLike[str]", str], clean: bool):
             f"Oups, the path for back file or folder ` {path}` is missing !"
         )
 
-    with Adb() as adb_session:
+    server = Adb(auto_start=True)
 
-        # wait for adb-server to detect phone
-        while adb_session.state is not adb.ConnectionState.CONNECTED:
-            sleep(1)
+    # wait for adb-server to detect phone
+    if server.state is not server.ConnectionState.CONNECTED:
+        click.echo("Device not connected.")
+        server.stop_server()
+        sys.exit(0)
+    log.info("Device connected")
 
-        cleanup_todo = []  # keep track of files/dir to delete before returning
+    cleanup_todo = []  # keep track of files/dir to delete before returning
 
-        if os.path.isdir(path):  # restore a folder back up
-            log.info(f"Restoring back up from path `{path}` *  *Folder*")
-            root_dir_back_up = path
+    if os.path.isdir(path):  # restore a folder back up
+        log.info(f"Restoring back up from path `{path}` *  *Folder*")
+        root_dir_back_up = path
 
-        elif (
-            os.path.isfile(path) and os.path.splitext(path)[1] == ".zip"
-        ):  # restore a zip archive back up
-            log.info(f"Restoring back up from path {path} *  *Zip*")
-            extract_to = os.path.splitext(path)[0]
-            unzipify(zip_file=path, dest_dir=extract_to)
-            root_dir_back_up = extract_to  # set as path root the folder with apk extracted from zip file
-            cleanup_todo = [extract_to, path]
+    elif (
+        os.path.isfile(path) and os.path.splitext(path)[1] == ".zip"
+    ):  # restore a zip archive back up
+        log.info(f"Restoring back up from path {path} *  *Zip*")
+        extract_to = os.path.splitext(path)[0]
+        unzipify(file=path, dest_dir=extract_to)
+        root_dir_back_up = extract_to  # set as path root the folder with apk extracted from zip file
+        cleanup_todo = [extract_to, path]
 
-        apks = [file for file in os.listdir(root_dir_back_up) if file.endswith(".apk")]
+    apks = [file for file in os.listdir(root_dir_back_up) if file.endswith(".apk")]
 
-        # calculate total installation size
-        size = [os.path.getsize(os.path.join(root_dir_back_up, apk)) for apk in apks]
+    # calculate total installation size
+    size = [os.path.getsize(os.path.join(root_dir_back_up, apk)) for apk in apks]
 
-        log.info(
-            "Total Installation Size: {0:.2f} MB".format(sum(size) / (1024 * 1024))
-        )
+    log.info(
+        "Total Installation Size: {0:.2f} MB".format(sum(size) / (1024 * 1024))
+    )
 
-        for progress, apk in enumerate(apks, 1):
-            log.info(f"[{progress}/{len(apks)}] Installing {apk}")
-            # FIXME: what ?
-            # adb_session.push(os.path.join(root_dir_back_up, apk))
+    for progress, apk in enumerate(apks, 1):
+        msg = f"[{progress}/{len(apks)}] Installing {apk}"
+        log.info(msg
+                 )
+        try:
+            server.push(os.path.join(root_dir_back_up, apk))
+        except AdbError as error:
+            click.echo("Error during pulling {0}\n{1}".format(apk, str(error)), err=True)
+            server.stop_server()
+            sys.exit(-1)
 
-        if clean:
-            for item in cleanup_todo:
-                if os.path.isdir(item):
-                    shutil.rmtree(item)
-                else:
-                    os.remove(item)
+    if clean:
+        for item in cleanup_todo:
+            if os.path.isdir(item):
+                shutil.rmtree(item)
+            else:
+                os.remove(item)
 
+    server.stop_server()
     log.info("Restore  done")
